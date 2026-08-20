a393b1e8a75115b8454da8dcbcba4e73df1da3c0
Rohit singh <157676773+Rohit7Be@users.noreply.github.com>
2026-08-20 16:59:18 +0530

    Time: 0 ms (100%), Space: 19.3 MB (49.79%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.py
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.py
@@ -2,13 +2,13 @@ class Solution:
     def minAddToMakeValid(self, s: str) -> int:
         openBrac = 0
         minAddBrac = 0
+
         for i in s:
             if i == "(":
-                openBrac +=1
+                openBrac += 1
             else:
                 if openBrac > 0:
                     openBrac -=1
                 else:
-                    minAddBrac +=1
+                    minAddBrac +=  1
         return openBrac + minAddBrac
-            
