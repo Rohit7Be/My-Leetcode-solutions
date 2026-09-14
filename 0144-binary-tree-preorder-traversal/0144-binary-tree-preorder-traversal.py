e478983696e304dc03eadb1e98c314b1d57261d1
Rohit singh <157676773+Rohit7Be@users.noreply.github.com>
2026-09-14 16:59:48 +0530

    Time: 0 ms (100%), Space: 19.3 MB (29.13%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
@@ -11,10 +11,9 @@ class Solution:
         def preorder(node):
             if node is None:
                 return
-            
             res.append(node.val)
             preorder(node.left)
             preorder(node.right)
-        
         preorder(root)
+
         return res
