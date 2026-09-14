a7b049ba2ccaa4e547c1cfc41bcd0a0f69e6906e
Rohit singh <157676773+Rohit7Be@users.noreply.github.com>
2026-09-14 17:20:49 +0530

    Time: 956 ms (5.27%), Space: 22.8 MB (60.66%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,7 +1,6 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        dic = {n: nums.count(n) for n in set(nums)}
+        res = sorted(dic.keys(), key=lambda x: (dic[x],x), reverse=True)
 
-        dic = {n : nums.count(n) for n in set(nums)}
-        maxF = sorted(dic.keys(), key=lambda x: (dic[x], x), reverse=True)
-
-        return maxF[:k]
+        return res[:k]
