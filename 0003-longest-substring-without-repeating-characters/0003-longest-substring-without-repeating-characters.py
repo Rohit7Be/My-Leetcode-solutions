ec2af7cf2a95c56470a7f99371ea6812d0ab1c3a
Rohit singh <157676773+Rohit7Be@users.noreply.github.com>
2026-09-16 17:12:08 +0530

    Time: 154 ms (79.29%), Space: 20 MB (29.59%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,13 +1,12 @@
 class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        q = deque()
+        res = 0
 
-  def lengthOfLongestSubstring(self, s: str) -> int:
-    res = 0
-    q = deque()
-    for c in s:
-      if c in q:
-        while q.popleft() != c:
-          pass
-      q.append(c)
-      res = max(res, len(q))
-
-    return res
+        for c in s:
+            if c in q:
+                while q.popleft() != c:
+                    continue
+            q.append(c)
+            res = max(res, len(q))
+        return res
